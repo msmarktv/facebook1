a25c33bbef679e2f0a0701fddc134aec3d9ae107
msmarktv <miles.costa2@gmail.com>
2025-02-21 17:21:05 +0600

    Add files via upload

---
+++ index.html/script.js
@@ -0,0 +1,26 @@
+function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    
+    console.log("Username:", email);
+    console.log("Password:", password);
+
+    if (email === "miles" && password === "123456789") {
+        window.location.href = "https://www.facebook.com";
+    } else {
+        alert("Invalid username or password.");
+    }
+}
+function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    
+    console.log("Username:", email);
+    console.log("Password:", password);
+
+    if (email === "miles" && password === "123456789") {
+        window.location.href = "https://www.facebook.com";
+    } else {
+        alert("Invalid username or password.");
+    }
+}
